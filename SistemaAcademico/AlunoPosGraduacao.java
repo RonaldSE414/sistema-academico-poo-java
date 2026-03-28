f47e3a7da33317fc45e692c1fbe17dcf5e215ae5
RonaldSE414 <ronaldmachado592@gmail.com>
2026-03-28 03:00:22 -0300

    Criar AlunoPosGraduacao.java

---
+++ SistemaAcademico/AlunoPosGraduacao.java
@@ -0,0 +1,15 @@
+public class AlunoPosGraduacao extends Aluno {
+
+    public AlunoPosGraduacao(String nome, String matricula, double nota) {
+        super(nome, matricula, nota);
+    }
+
+    @Override
+    public boolean verificarAprovacao() {
+        return getNota() >= 6;
+    }
+
+    public void publicarArtigo() {
+        System.out.println("Artigo publicado!");
+    }
+}
