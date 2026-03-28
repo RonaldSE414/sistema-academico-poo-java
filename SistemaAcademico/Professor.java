8fd9bb147e2617f57fbdcaabdbe7047d64877357
RonaldSE414 <ronaldmachado592@gmail.com>
2026-03-28 03:00:42 -0300

    Criar Professor.java

---
+++ SistemaAcademico/Professor.java
@@ -0,0 +1,28 @@
+public class Professor {
+    private String nome;
+    private String matricula;
+    private double salario;
+
+    public Professor(String nome, String matricula, double salario) {
+        this.nome = nome;
+        this.matricula = matricula;
+
+        if (salario < 0) {
+            this.salario = 0;
+        } else {
+            this.salario = salario;
+        }
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void aumentarSalario(double valor) {
+        if (valor > 0) {
+            salario += valor;
+        } else {
+            System.out.println("Valor inválido!");
+        }
+    }
+}
